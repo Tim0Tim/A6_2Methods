f5d34c78fc00a3221dd1975e47752b78dff83e75
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-04-11 08:07:44 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A6_2Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
