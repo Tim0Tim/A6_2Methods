19452f41b4974bb96ae9ddfb3dc987b4bc9ac1f1
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-04-11 08:46:06 -0600

    Near completion

--- Program.cs
+++ Program.cs
@@ -4,9 +4,51 @@ namespace A6_2Methods
 {
     class Program
     {
-        static void Main(string[] args)
+        public double ce;
+        public double f;
+        public static double Main()
         {
-            Console.WriteLine("Hello World!");
+            var calc = new Program();
+            double ce;
+            double f;
+            int T;
+            double num = 32;
+            Console.WriteLine("Do you want to convert Celsius to Fahrenheit(1) \n--or--\n Convert Fahrenheit to Celsius(2)");
+            T = int.Parse(Console.ReadLine());
+
+
+            if (T == 1)
+            {
+                Console.WriteLine("What is the Fahrenheit value?");
+                f = double.Parse(Console.ReadLine());
+                Console.WriteLine("The Value is:");
+                return calc.Cel(f,num);
+                Console.Write(" °C");
+            }
+            else if(T == 2)
+            {
+                Console.WriteLine("What is the Celcius value?");
+                ce = double.Parse(Console.ReadLine());
+                Console.WriteLine("the Value is:");
+                return calc.Fa(ce,num);
+                Console.Write(" °F");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                return null;
+            }
+        }
+
+        public double Cel(double f, double num)
+        {
+         return 5.0/9.0 * f - num;
+        
+        }
+        
+        public double Fa(double ce, double num)
+        {
+            return 9.0 / 5.0 * ce + num;
         }
     }
 }
